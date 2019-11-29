e67f534c4f4d7f73456cfd25b7ac1ac69f7b6f10
wahyudi <wahyudi7104@gmail.com>
2019-11-29 20:17:36 +0700

    memperbaharui file html dan menambahkan beberapa conditional dan convert string pada file index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function buttonForLogin() {
-    document.getElementById('formLogin').style.display = "none";
-    document.getElementById('bmiOrCalori').style.display = "block";  
+    var userName = document.getElementById('name').value
+    if (userName == "") {
+        alert("Form nama belum diisi")
+    } else {
+        document.getElementById('userName').innerHTML = "Selamat datang " + userName
+        document.getElementById('formLogin').style.display = "none";
+        document.getElementById('bmiOrCalori').style.display = "block"; 
+    } 
 }
 
 function buttonForBmi() {
@@ -9,38 +15,53 @@ function buttonForBmi() {
     var hasil = beratBadan / (tinggiBadan * tinggiBadan)
     var showHasil = document.getElementById("showHasil")
 
-    if (hasil >= 17.00 && hasil <= 18.50) {
-        showHasil.innerHTML ="Anda kekurangan berat badan"
-    } else if (hasil >= 16.00 && hasil <= 16.99) {
-        showHasil.innerHTML = "Anda kekurangan berat badan, disarankan makan makanan yang bergizi"
-    } else if (hasil <= 15.99){
-        showHasil.innerHTML = "Anda kekurangan berat badan, disarankan makan makanan yang bergizi dan jangan begadang"
-    }
-    
-    if(hasil >= 18.5 && hasil <= 24.9) {
-        showHasil.innerHTML = "Berat badan anda normal"
-    }
-    
-    if (hasil >=25.00 && hasil <= 30.00) {
-        showHasil.innerHTML = "Anda kelebihan berat badan"
-    } else if (hasil >=30.00 && hasil <= 34.99){
-        showHasil.innerHTML = "Anda obesitas, disarankan mengurangi makanan yang mengandung gula berlebih"
-    } else if (hasil >= 35.00){
-        showHasil.innerHTML = "Anda obesitas, disarankan mengurangi makanan yang mengandung gula berlebih dan harus mulai berolahraga"
+    if (beratBadan == "") {
+        alert("Berat badan belum diisi")
+    } else if (tinggiBadan == "") {
+        alert("Tinggi badan belum diisi")
+    } else {
+        if (hasil >= 17.00 && hasil <= 18.50) {
+            showHasil.innerHTML = "Anda kekurangan berat badan"
+        } else if (hasil >= 16.00 && hasil <= 16.99) {
+            showHasil.innerHTML = "Anda kekurangan berat badan, disarankan makan makanan yang bergizi"
+        } else if (hasil <= 15.99) {
+            showHasil.innerHTML = "Anda kekurangan berat badan, disarankan makan makanan yang bergizi dan jangan begadang"
+        }
+
+        if (hasil >= 18.5 && hasil <= 24.9) {
+            showHasil.innerHTML = "Berat badan anda normal"
+        }
+
+        if (hasil >= 25.00 && hasil <= 30.00) {
+            showHasil.innerHTML = "Anda kelebihan berat badan"
+        } else if (hasil >= 30.00 && hasil <= 34.99) {
+            showHasil.innerHTML = "Anda obesitas, disarankan mengurangi makanan yang mengandung gula berlebih"
+        } else if (hasil >= 35.00) {
+            showHasil.innerHTML = "Anda obesitas, disarankan mengurangi makanan yang mengandung gula berlebih dan harus mulai berolahraga"
+        }
     }
 }
 
 function buttonForCalori() {
-    var yangSayaMakan = document.getElementById("makananSaya").Value
-    var makanan = {
-        nasiPutih: "175 kalori", lontong: "38 kalori", ketupat: "32 kalori", jagungRebus: "90,2 kalori", kentangRebus: "166 kalori", 
-        ketanPutih: "217 kalori", rotiTawar: "128 kalori", ubiRebus: "125 kalori", bubur: "44 kalori", mieInstan: "168 kalori", nasiUduk: "506 kalori",
-        buburAyam: "165 kalori", mieGoreng: "321 kalori", nasiGoreng: "267 kalori", spaghetti: "642 kalori", asinan: "208", ketoprak: "153 kalori",
-        sotoAyam: "101 kalori", rawon: "331 kalori", sotoPadang: "127 kalori", hamburger: "257 kalori", mieBakso: "302 kalori", pizza: "163 kalori", 
-        sateKambing: "729 kalori", siomay: "361 kalori", ayamPanggang: "164,3 kalori", empalDaging: "147 kalori", ikanLeleGoreng: "57,5 kalori",
-        ikanPatinGoreng: "252,7 kalori", ikanTeriGoreng: "66 kalori", kerangRebus: "59 kalori", telurMataSapi: "40 kalori", telurDadar: "188 kalori",
-        tempeBacem: "157 kalori", tempeGoreng: "118 kalori", sopSapi: "227 kalori", tahuGoreng: "111 kalori", tahuSumedang: "113 kalori",
-        sayurAsam: "88 kalori", sopBayam: "78 kalori", gudeg: "132 kalori", sayurLodeh: "61 kalori", tumisBuncis: "52 kalori", tumisDaunSingkong: "151 kalori",
-    };
-    document.getElementById("showMakanan").innerHTML = makanan.yangSayaMakan;
+    var jumlahKalori = document.getElementById("makananSaya").value
+    
+    var result = jumlahKalori.replace(/[^\w\s]/g, "")
+    result = result.replace(/\s+/g," ")
+    jumlahKalori = result.toLowerCase().split(' ').join('_')
+
+    if (jumlahKalori == "") {
+        alert ("Makanan belum diisi")
+    } else {
+        var makanan = {
+            nasi: "175 kalori", lontong: "38 kalori", ketupat: "32 kalori", jagung_rebus: "90,2 kalori", kentang_rebus: "166 kalori",
+            ketan_putih: "217 kalori", roti_tawar: "128 kalori", ubi_rebus: "125 kalori", bubur: "44 kalori", mie_instan: "168 kalori", nasi_uduk: "506 kalori",
+            bubur_ayam: "165 kalori", mie_goreng: "321 kalori", nasi_goreng: "267 kalori", spaghetti: "642 kalori", asinan: "208", ketoprak: "153 kalori",
+            soto_ayam: "101 kalori", rawon: "331 kalori", soto_padang: "127 kalori", hamburger: "257 kalori", mie_bakso: "302 kalori", pizza: "163 kalori",
+            sate_kambing: "729 kalori", siomay: "361 kalori", ayam_panggang: "164,3 kalori", empal_daging: "147 kalori", ikan_lele_goreng: "57,5 kalori",
+            ikan_patin_goreng: "252,7 kalori", ikan_teri_goreng: "66 kalori", kerang_rebus: "59 kalori", telur_mata_sapi: "40 kalori", telurDadar: "188 kalori",
+            tempe_bacem: "157 kalori", tempe_goreng: "118 kalori", sop_sapi: "227 kalori", tahu_goreng: "111 kalori", tahu_sumedang: "113 kalori",
+            sayur_asam: "88 kalori", sop_bayam: "78 kalori", gudeg: "132 kalori", sayur_lodeh: "61 kalori", tumis_buncis: "52 kalori", tumis_daun_singkong: "151 kalori",
+        }
+    }
+    document.getElementById("showMakanan").innerHTML = makanan[jumlahKalori]
 }
